11b5e2a7d09a991952195f4ad21a3526b6c2cf4f
WoLewicki <lewywojt@gmail.com>
2019-06-12 19:07:24 +0200

    Testing background image

--- src/components/Screens/Tutorial/TutorialMenu.js
+++ src/components/Screens/Tutorial/TutorialMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Platform, StyleSheet, Text, View, ImageBackground } from 'react-native';
 import styles from '../../../styles/Menu/MenuMainStyle'
 
 import MenuButton from '../../Buttons/MenuButton';
@@ -8,8 +8,10 @@ export default class Training extends Component {
     render() {
         const {navigate} = this.props.navigation;
         return (
-            <View style={styles.container}>
+            <View >
+                <ImageBackground source={{uri: 'http://lenduletmagazin.hu/wp-content/uploads/2016/07/u8PEixm.jpg'}} style={{width: '100%', height: '100%'}}>
                 <MenuButton text='Main screen' onPress={() => navigate('Menu')}/>
+                </ImageBackground>
             </View>
         );
     }
