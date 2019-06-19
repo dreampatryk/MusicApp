2ea2ae45603a7f8659500bbc5ff0405fd46a3c8e
patryktraveler <patryktraveler@gmail.com>
2019-06-19 19:00:49 +0200

    Added falling notes

--- src/components/Screens/Game/Level.js
+++ src/components/Screens/Game/Level.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View, Button, Orientation } from 'react-native';
+import { Image, StyleSheet, View, Animated, Dimensions } from 'react-native';
 //import styles from '../../../styles/Menu/MenuMainStyle'
 
 import MenuButton from '../../Buttons/MenuButton';
 import TutorialButton from "../../Buttons/TutorialButton";
 import TutorialTexts from "../../../styles/Texts/TutorialTexts";
 import Piano from '../../Piano/Piano';
+import Board from './Board';
 
 
 
 export default class Level extends Component {
     state = {
         noteIndex: 0,
+        movingVal: new Animated.Value(0)
     };
     notesLength = this.props.navigation.getParam('notesLength', 0);
     notes       = this.props.navigation.getParam('notes', []);
@@ -21,12 +23,30 @@ export default class Level extends Component {
 
     onStop = (note) => this.pianoElement.current.simulateOnTouchEnd(note);
 
+    moveNotes(){
+        setInterval(() => {
+            //console.warn(this.state.movingVal._value)
+        }, 100)
+
+        Animated.timing(
+            this.state.movingVal,
+            {
+                toValue: Dimensions.get('window').height,
+                duration: 3000
+            }
+        ).start();
+    }
+
+    componentDidMount(){
+        this.moveNotes();
+    }
+
     render() {
         const {navigation} = this.props;
         return (
             <View style={styles.container}>
-                <View style={{width: 100, height: 100, backgroundColor: 'red'}} onTouchStart={() => this.onPlay('c4')} onTouchEnd={() => this.onStop('c4')}/>
-                <Piano ref={this.pianoElement} noteRange={{first: 'c4', last: 'c#6'}} onPlayNoteInput = {this.onPlay} onStopNoteInput = {this.onStop}></Piano>
+                <Board noteRange={{first: 'c4', last: 'c#6'}} startPos={0} movingVal={this.state.movingVal}/>
+                
             </View>
         );
     }
@@ -36,7 +56,7 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'flex-end',
     flex: 1, 
-    backgroundColor: 'green',
+    backgroundColor: 'black',
   }
 })
 
