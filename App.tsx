cbf84118578d1fe6deb6887a4a134d8d53b42de7
WoLewicki <lewywojt@gmail.com>
2019-07-07 16:53:08 +0200

    added working prettier

--- App.tsx
+++ App.tsx
@@ -1,31 +1,32 @@
-import {createStackNavigator, createAppContainer} from 'react-navigation';
-
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import Level from './src/components/Screens/Game/Level';
+import StartGameMenu from './src/components/Screens/Game/StartGameMenu';
 import MenuScreen from './src/components/Screens/MainMenu/MenuScreen';
-import Training from "./src/components/Screens/Training/Training";
-import TutorialMenu from "./src/components/Screens/Tutorial/TutorialMenu";
-import Tutorial from "./src/components/Screens/Tutorial/Tutorial";
-import StartGameMenu from "./src/components/Screens/Game/StartGameMenu";
-import Level from "./src/components/Screens/Game/Level";
+import Training from './src/components/Screens/Training/Training';
+import Tutorial from './src/components/Screens/Tutorial/Tutorial';
+import TutorialMenu from './src/components/Screens/Tutorial/TutorialMenu';
 
-const MainNavigator = createStackNavigator({
-  Menu: {screen: MenuScreen},
-  Training: {screen: Training},
-  TutorialMenu: {screen: TutorialMenu},
-  Tutorial: {screen: Tutorial},
-  StartGameMenu: {screen: StartGameMenu},
-  Level: {screen: Level}
-},
-{
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: 'red',
+const MainNavigator = createStackNavigator(
+  {
+    Menu: { screen: MenuScreen },
+    Training: { screen: Training },
+    TutorialMenu: { screen: TutorialMenu },
+    Tutorial: { screen: Tutorial },
+    StartGameMenu: { screen: StartGameMenu },
+    Level: { screen: Level },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: 'red',
+      },
       headerTintColor: '#fff',
       headerTitleStyle: {
         fontWeight: 'bold',
       },
-    }
-  }
-});
+    },
+  },
+);
 
 const App = createAppContainer(MainNavigator);
 
